9fd89704b90986465de73fd95f78fec0e7a25845
Wollaston <matt@wollaston.dev>
2024-11-21 19:46:02 -0500

    Init

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import MyComponent from './MyComponent';
+import Test from './Test';
 
 function App() {
   return (
     <div className="App">
+      <Test />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
@@ -17,7 +20,17 @@ function App() {
         >
           Learn React
         </a>
+        <h1 className="green-500 text-3xl font-bold">Hello World!</h1>
+        <h1 className='slate-500'>Test</h1>
       </header>
+      <MyComponent />
+      <MyComponent />
+      <MyComponent />
+      <MyComponent />
+      <MyComponent />
+      <MyComponent />
+      <MyComponent />
+      <MyComponent />
     </div>
   );
 }
---
+++ src/MyComponent.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import Test from './Test';
+
+function MyComponent() {
+
+  return (
+    <div className="App">
+      <h1>Hello from OurComponent!</h1>
+      <Test />
+      <Test />
+      <Test />
+    </div>
+  );
+}
+
+export default MyComponent;
